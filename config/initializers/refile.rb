a66013ee2b49d3efc5451e22aa3814806d55ca88
Fcott <f.f.f.kt13@gmail.com>
2017-01-03 18:28:11 +0900

    refile_s3 is only for product

---
+++ config/initializers/refile.rb
@@ -0,0 +1,12 @@
+require "refile/s3"
+
+if Rails.env.production?
+  aws = {
+    access_key_id: ENV["AWS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_KEY"],
+    region: ENV["AWS_REGION"],
+    bucket: ENV["AWS_BUCKET"],
+  }
+  Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+  Refile.store = Refile::S3.new(prefix: "store", **aws)
+end
