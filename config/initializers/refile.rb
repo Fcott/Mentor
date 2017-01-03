731b73699678f8a1449aeb238d9fec74b75cd667
Fcott <f.f.f.kt13@gmail.com>
2017-01-03 21:52:19 +0900

    correct refile.rb syntax

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
     access_key_id: ENV["AWS_KEY_ID"],
     secret_access_key: ENV["AWS_SECRET_KEY"],
     region: ENV["AWS_REGION"],
-    bucket: ENV["AWS_BUCKET"],
+    bucket: ENV["AWS_BUCKET"]
   }
   Refile.cache = Refile::S3.new(prefix: "cache", **aws)
   Refile.store = Refile::S3.new(prefix: "store", **aws)
