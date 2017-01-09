50e735024cf93ba7ffcd041358a852cb8283de01
Fcott <f.f.f.kt13@gmail.com>
2017-01-09 23:13:13 +0900

    conversation partial and UI for not logged in user

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,5 +1,5 @@
 class StoriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :show
   before_action :correct_user, only: :destroy
 
   def show
