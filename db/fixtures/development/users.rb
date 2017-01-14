c6cc70641129e3b2ae207c7ea1a40f414865bd63
Fcott <f.f.f.kt13@gmail.com>
2017-01-14 23:29:04 +0900

    little fix

--- db/fixtures/development/users.rb
+++ db/fixtures/development/users.rb
@@ -1,10 +1,10 @@
 User.seed do |s|
-  s.id: 1,
-  s.email: "",
-  s.created_at: "2017-01-08 05:04:44",
-  s.updated_at: "2017-01-08 05:07:38",
-  s.provider: nil,
-  s.uid: nil,
-  s.username: "",
-  s.image: nil
+  # s.id: 1,
+  # s.email: "",
+  # s.created_at: "2017-01-08 05:04:44",
+  # s.updated_at: "2017-01-08 05:07:38",
+  # s.provider: nil,
+  # s.uid: nil,
+  # s.username: "",
+  # s.image: nil
 end
