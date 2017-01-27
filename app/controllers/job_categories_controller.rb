186414dd4989a7454c48130d69d60e4c66ccccbc
Fcott <f.f.f.kt13@gmail.com>
2017-01-27 10:54:08 +0900

    add subcategories in view and controller

--- app/controllers/job_categories_controller.rb
+++ app/controllers/job_categories_controller.rb
@@ -1,5 +1,7 @@
 class JobCategoriesController < ApplicationController
   def show
     @job_category = JobCategory.find(params[:id])
+    @job_stories = Story.all
+    @subcategories = @job_category.subcategories
   end
 end
