ed818a5cc2599f6614e2fa6617b4e324d4df4e72
Fcott <f.f.f.kt13@gmail.com>
2017-01-27 14:14:49 +0900

    correct syntax

--- app/models/user_job.rb
+++ app/models/user_job.rb
@@ -1,5 +1,5 @@
 class UserJob < ApplicationRecord
-  validates :job_category_id, uniqueness: scope: :user_id
+  validates :job_category_id, uniqueness: {scope: :user_id}
   belongs_to :user
   belongs_to :job_category
 
